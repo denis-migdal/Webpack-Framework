168bf0a844af65150a311d49bdb7029cf660103c
denmigda <denis.migdal@uca.fr>
2021-07-14 09:57:48 +0200

    Fix Typescript

--- src/rules/ts.js
+++ src/rules/ts.js
@@ -2,15 +2,16 @@ const fs = require('fs');
 
 module.exports = function(config, {ROOT, src, dst, args, optimize}) {
 
+	config.module.rules.push({
+		test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+	});
+
 	if(! fs.existsSync(`${ROOT}/${src}/index.ts`) )
 		return;
 
 	config.output.filename = `${dst}/index.js`;
 	config.entry.push(`${src}/index.ts`);
 
-	config.module.rules.push({
-		test: /\.tsx?$/,
-        use: 'ts-loader',
-        exclude: /node_modules/
-	});
 }
